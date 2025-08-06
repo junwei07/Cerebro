05a202c684b603b14058fc63790a863853e1f261
LY2177 <liangliyee2004@gmail.com>
2025-08-06 13:26:42 +0800

    Initial Commit

---
+++ Cerebro/CerebroApp.swift
@@ -0,0 +1,17 @@
+//
+//  CerebroApp.swift
+//  Cerebro
+//
+//  Created by Interactive 3D Design Group on 6/8/25.
+//
+
+import SwiftUI
+
+@main
+struct CerebroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Cerebro/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Cerebro
+//
+//  Created by Interactive 3D Design Group on 6/8/25.
+//
+
+import SwiftUI
+import RealityKit
+import RealityKitContent
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Model3D(named: "Scene", bundle: realityKitContentBundle)
+                .padding(.bottom, 50)
+
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview(windowStyle: .automatic) {
+    ContentView()
+}
